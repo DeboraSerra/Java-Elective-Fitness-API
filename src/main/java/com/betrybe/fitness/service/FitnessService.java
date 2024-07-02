e5aca5945d153a58891acad118af974d962b762a
Débora <debora.r.serra@gmail.com>
2024-07-02 14:31:59 -0300

    feat: add req 7

--- src/main/java/com/betrybe/fitness/service/FitnessService.java
+++ src/main/java/com/betrybe/fitness/service/FitnessService.java
@@ -11,25 +11,38 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FitnessService implements FitnessServiceInterface {
-  private FakeFitnessDatabase database;
+
+  private final FakeFitnessDatabase database;
 
   @Autowired
   public FitnessService(FakeFitnessDatabase database) {
-    this.database = database
+    this.database = database;
+  }
+
+  private WorkoutDto transformWorkoutToWotkoutDTO(Workout workout) {
+    WorkoutDto result = new WorkoutDto(workout.getId(), workout.getName(),
+        workout.getRepetitions());
+    return result;
   }
 
   @Override
   public WorkoutDto saveWorkout(WorkoutCreationDto newWorkoutDto) {
-    return null;
+    Workout workout = new Workout();
+    workout.setName(newWorkoutDto.name());
+    workout.setRepetitions(newWorkoutDto.repetitions());
+    workout.setSecretTechnique(newWorkoutDto.secretTechnique());
+    Workout response = database.saveWorkout(workout);
+    WorkoutDto workoutDto = transformWorkoutToWotkoutDTO(response);
+    return workoutDto;
   }
 
   @Override
   public Optional<WorkoutDto> getWorkout(Long id) {
     Optional<Workout> result = database.getWorkout(id);
-    if(result.isEmpty()) {
+    if (result.isEmpty()) {
       return Optional.empty();
     }
-    WorkoutDto workout = new WorkoutDto(result.get().getId(), result.get().getName(), result.get().getRepetitions());
+    WorkoutDto workout = transformWorkoutToWotkoutDTO(result.get());
     return Optional.of(workout);
   }
 
