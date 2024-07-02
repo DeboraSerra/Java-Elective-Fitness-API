1207ddffc7c16d9850c140b599bcf4bd34d6e7f1
Débora <debora.r.serra@gmail.com>
2024-07-02 14:35:48 -0300

    feat: add req 8

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,12 +1,16 @@
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessService;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +37,11 @@ public class FitnessController implements FitnessControllerInterface {
       return ResponseEntity.notFound().build();
     }
     return ResponseEntity.ok(workout);
+  }
+
+  @PostMapping("/workouts")
+  public ResponseEntity<WorkoutDto> createWorkout(@RequestBody WorkoutCreationDto workout) {
+    WorkoutDto response = service.saveWorkout(workout);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 }
