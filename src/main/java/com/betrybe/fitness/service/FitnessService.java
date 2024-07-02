73b919c85c2eb38b8876025f08e3c33bfda048c6
Débora <debora.r.serra@gmail.com>
2024-07-02 14:51:58 -0300

    feat: add req 9

--- src/main/java/com/betrybe/fitness/service/FitnessService.java
+++ src/main/java/com/betrybe/fitness/service/FitnessService.java
@@ -4,6 +4,7 @@ import com.betrybe.fitness.database.FakeFitnessDatabase;
 import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.model.Workout;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +57,11 @@ public class FitnessService implements FitnessServiceInterface {
 
   @Override
   public List<WorkoutDto> getAllWorkouts() {
-    return List.of();
+    List<Workout> workouts = database.getAllWorkouts();
+    ArrayList<WorkoutDto> workoutDtos = new ArrayList<WorkoutDto>();
+    for (Workout workout : workouts) {
+      workoutDtos.add(new WorkoutDto(workout.getId(), workout.getName(), workout.getRepetitions()));
+    }
+    return workoutDtos;
   }
 }
